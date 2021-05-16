8c69f9945680a8a9ef52b5bc8c3b47bca0cf2d58
Your Name <you@example.com>
2021-05-16 13:28:02 +0100

    El-mahdi add something

---
+++ src/app/controller/service/categorie-tache.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {CategorieTache} from '../model/categorie-tache.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategorieTacheService {
+  private url = environment.baseUrl + 'categorieTache/';
+  private _items: Array<CategorieTache>;
+
+  constructor(private http: HttpClient) {
+  }
+
+  public findAll(): Observable<Array<CategorieTache>> {
+    return this.http.get<Array<CategorieTache>>(this.url);
+  }
+
+  get items(): Array<CategorieTache> {
+    return this._items;
+  }
+
+  set items(value: Array<CategorieTache>) {
+    this._items = value;
+  }
+}
+
