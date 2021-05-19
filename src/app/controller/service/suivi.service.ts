c15f5bec74205dac860ab0d869126d8b2d2a6fd2
Your Name <you@example.com>
2021-05-19 18:40:09 +0100

    El-mahdi add something

--- src/app/controller/service/suivi.service.ts
+++ src/app/controller/service/suivi.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CollaborateurVo} from '../model/collaborateur-vo.model';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SuiviService {
 
-  constructor() { }
+    private url = environment.baseUrl + 'tache/';
+
+
+    private _selected: CollaborateurVo;
+    private _items: Array<CollaborateurVo>;
+
+    constructor(private http: HttpClient) {
+    }
+
+    get selected(): CollaborateurVo {
+        if (this._selected == null) {
+            this._selected = new CollaborateurVo();
+        }
+        return this._selected;
+    }
+
+    set selected(value: CollaborateurVo) {
+        this._selected = value;
+    }
+
+    get items(): Array<CollaborateurVo> {
+        if (this._items == null) {
+            this._items = new Array<CollaborateurVo>();
+        }
+        return this._items;
+    }
+
+    set items(value: Array<CollaborateurVo>) {
+        this._items = value;
+    }
+
+    public calcStatistiqueSuiviCollaborateur(): Observable<Array<CollaborateurVo>> {
+        console.log(this.url + 'dateMin/' + this.selected.dateDemarrageEffectiveMin
+            + '/dateMax/' + this.selected.dateDemarrageEffectiveMax);
+        return this.http.get<Array<CollaborateurVo>>(this.url + 'dateMin/'
+            + this.selected.dateDemarrageEffectiveMin
+            + '/dateMax/' + this.selected.dateDemarrageEffectiveMax);
+    }
+
 }
--- src/app/view/admin/component/statistiques-suivi-collaborateur/criteres-de-recherche/criteres-de-recherche.component.ts
+++ src/app/view/admin/component/statistiques-suivi-collaborateur/criteres-de-recherche/criteres-de-recherche.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SuiviService} from '../../../../../controller/service/suivi.service';
+import {CollaborateurVo} from '../../../../../controller/model/collaborateur-vo.model';
+import {GroupeTache} from '../../../../../controller/model/groupe-tache.model';
 
 @Component({
-  selector: 'app-criteres-de-recherche',
-  templateUrl: './criteres-de-recherche.component.html',
-  styleUrls: ['./criteres-de-recherche.component.scss']
+    selector: 'app-criteres-de-recherche',
+    templateUrl: './criteres-de-recherche.component.html',
+    styleUrls: ['./criteres-de-recherche.component.scss']
 })
 export class CriteresDeRechercheComponent implements OnInit {
 
-  constructor() { }
+    constructor(private service: SuiviService) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    get selected(): CollaborateurVo {
+        return this.service.selected;
+    }
+
+    get items(): Array<CollaborateurVo> {
+        return this.service.items;
+    }
+
+    set items(value: Array<CollaborateurVo>) {
+        this.service.items = value;
+    }
+
+    calcStatistiqueSuiviCollaborateur() {
+        this.service.calcStatistiqueSuiviCollaborateur().subscribe(data => this.items = data);
+    }
 
 }
