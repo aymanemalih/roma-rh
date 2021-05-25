901c345d3a184093657ca3b0357246f226f6faec
Your Name <you@example.com>
2021-05-25 21:39:08 +0100

    El-mahdi add something

---
+++ src/app/controller/model/paiement.model.ts
@@ -0,0 +1,15 @@
+import {Facture} from './facture.model';
+import {TypePaiement} from './type-paiement.model';
+import {EtatPaiement} from './etat-paiement.model';
+
+export class Paiement {
+    public id: number;
+    public libelle: string;
+    public code: string;
+    public reference: string;
+    public montant: number;
+    public datePaiement: Date;
+    public facture: Facture;
+    public etatPaiement: EtatPaiement;
+    public typePaiement: TypePaiement;
+}
